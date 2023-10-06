5d51276c6b4cb3faa7dc7547a9b6a02e2abe547e
Seong Hyun Kang <ruanelroem@gmail.com>
2023-10-06 15:42:08 +0900

    Initial commit

---
+++ Source/myTPSProject/private/EnemyManager.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyManager.h"
+#include "Enemy.h"
+
+// Sets default values
+AEnemyManager::AEnemyManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	
+}
+
+// Called when the game starts or when spawned
+void AEnemyManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	float createTime = FMath::RandRange(minTime, maxTime);
+	GetWorld()->GetTimerManager().SetTimer(spawnTimerHandle, this, &AEnemyManager::CreateEnemy, createTime);
+}
+
+void AEnemyManager::CreateEnemy()
+{	
+	int index = FMath::RandRange(0, spawnPoints.Num() - 1);
+	GetWorld()->SpawnActor<AEnemy>(enemyFactory, spawnPoints[index]->GetActorLocation(), FRotator(0));
+
+	float createTime = FMath::RandRange(minTime, maxTime);
+	GetWorld()->GetTimerManager().SetTimer(spawnTimerHandle, this, &AEnemyManager::CreateEnemy, createTime);
+}
+
+// Called every frame
+void AEnemyManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
